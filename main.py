b2a45ac22018f948e8cba9f8f795d57f2019d2d2
Igalto <itolch@mail,ru>
2025-01-29 15:34:15 +0300

    Make changes in script main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from read_data_from_file import read_data
 from write_data_to_file import write_data
-from db_query import get_query_db
+from db_query import get_query_db, create_table
 
 def total_revenue(tbl_name):
     query = 'SELECT sum(price * quantity) AS total_revenue FROM %s;' % (tbl_name)
@@ -12,7 +12,7 @@ def total_revenue(tbl_name):
 def items_by_category(tbl_name):
     query = 'SELECT category, array_agg(DISTINCT item) AS items_by_categories FROM %s GROUP BY 1 ORDER BY 1;' % (tbl_name)
     answers = get_query_db(query)
-    res = dict()
+    res = {}
     for answer in answers:
         res[answer[0]] = answer[1]
     print(f'Товары по категориям: {res}')
@@ -44,7 +44,7 @@ def average_price_by_category(tbl_name):
     answers = get_query_db(query)
     res = []
     for answer in answers:
-        x={}
+        x = {}
         x[answer[0]] = answer[1]
         res.append(x)
         
@@ -60,10 +60,29 @@ def most_frequent_category(tbl_name):
 
 
 if __name__ == '__main__':
+    # Считываем данные
+    file_data = 'data.txt'
+    my_data = read_data(file_data)
+    
+    # Выбираем название для таблицы в базе данных 'purchases_db'
     table_name = 'purchases'
+    
+    # Создаем таблицу 'purchases'
+    create_table(table_name, my_data)
+    
+    # Рассчитываем и возвращаем общую выручку (цена * количество для всех записей)
     total_revenue(table_name)
+    
+    # Возвращаем словарь, где ключ — категория, а значение — список уникальных товаров в этой категории
     items_by_category(table_name)
-    expensive_purchases(table_name)
+    
+    # Выводим все покупки, где цена товара больше или равна min_price
+    price_min = 1.0
+    expensive_purchases(table_name, price_min)
+    
+    # Рассчитываем среднюю цену товаров по каждой категории
     average_price_by_category(table_name)
+    
+    # Находим и возсращаем категорию, в которой куплено больше всего единиц товаров
     most_frequent_category(table_name)
     
